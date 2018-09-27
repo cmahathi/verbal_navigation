05bbd88b9c60752511e3393eec2634521eabc341
Jacqueline Deans <jdeans289@gmail.com>
2018-09-27 17:09:55 -0500

    set up a node

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include <stdio.h>
+#include "std_msgs/String.h"
 #include "ros/ros.h"
 
-int main () {
+int main (int argc, char** argv) {
 	printf("Hello world");
+	ros::init(argc, argv, "verbal_navigation");
+	ros::NodeHandle node;
+
+	while (ros::ok()) {
+		ROS_INFO("Hell world");
+	}
 }
