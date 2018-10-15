efaa89ae64bd3fa80757bfbdd5bfec5ee35c21ee
c6321 <c-she@utexas.edu>
2018-10-15 16:56:52 -0500

    Added ability to load locations from map.

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,9 @@
 #include <geometry_msgs/PoseWithCovarianceStamped.h>
 #include <tf/tf.h>
 #include <tf/transform_listener.h>
+#include <bwi_planning_common/structures.h>
+#include <bwi_logical_translator/bwi_logical_translator.h>
+
 
 
 class FuturePoseStamped {
@@ -49,9 +52,29 @@ public:
 	 }
 };
 
+void getRegion(FuturePoseStamped current_location) {
+	std::string locationFileName = "/home/fri/catkin_ws/src/verbal_navigation/src/3ne/locations.yaml";
+	std::vector<std::string> locations;
+	std::vector<int32_t> location_map;
+
+	bwi_planning_common::readLocationFile(locationFileName, locations, location_map);
+
+	ROS_INFO("Size of locations: %d, and size of map: %d\n", locations.size(), location_map.size());   	
+
+	tf::TransformListener tf;
+	geometry_msgs::PoseStamped out;
+	tf.transformPose("level_mux_map", current_location.getPose(), out);
+	float robot_x = out.pose.position.x;
+	float robot_y = out.pose.position.y;
+	
+	//Convert to cv::point2f using bwi_mapper::Point2f(robot_x, robot_y)
+	//Call bwi_logical_translator::getLocationIdx(cv::Point2f&);
+}
 
 int main (int argc, char** argv) {
 
+	
+	
 	FuturePoseStamped initialPose;
 	FuturePoseStamped goalPose;
 
@@ -61,7 +84,9 @@ int main (int argc, char** argv) {
 	ros::Subscriber sub = n.subscribe("/initialpose", 100, &FuturePoseStamped::setFromPoseWithCovarianceStamped, &initialPose);
 	ros::Subscriber sub1 = n.subscribe("/move_base_interruptable_simple/goal", 100, &FuturePoseStamped::setFromPoseStamped, &goalPose);
 
-	while(!(initialPose.isAvailable() && goalPose.isAvailable())) ros::spinOnce();
+
+	
+	while(!(initialPose.isAvailable() && goalPose.isAvailable()) && ros::ok()) ros::spinOnce();
 	ROS_INFO("HAVE START AND GOAL");
 	ros::ServiceClient path_client = n.serviceClient <nav_msgs::GetPlan> ("move_base/NavfnROS/make_plan");
 	path_client.waitForExistence();
@@ -76,22 +101,15 @@ int main (int argc, char** argv) {
 	path_client.call(srv);
 	ROS_INFO("Response received, size %d", srv.response.plan.poses.size());
 
+	// TODO: pass srv.response into new function (compartmentalize!!)
 
-
-
-	// BWI infrastructure will already be running, so just get the current location
-	// subscribe to odom
 
 
 	// there's a list of doors (which are coordinates). Figure out how
 	// that list is stored, and then pick a door from the list.
 
 
-	// finally, make sure we include movebase package, and then
-	// call move_base.make_plan with the start and end pose.
 
-
-	// call moveBase.make_plan
 
 
 	while (ros::ok()) {
