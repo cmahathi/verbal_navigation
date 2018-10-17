f0d8e5b2a9c1484e8c7174571bbb38b6a1ad3f7c
fri <jacqueline.gibson@utexas.edu>
2018-10-17 13:47:53 -0500

    aaaaahhhhhhhhhhh

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "std_msgs/String.h"
 #include "ros/ros.h"
 #include "nav_msgs/GetPlan.h"
@@ -14,12 +15,17 @@
 
 
 
-void getRegion(bwi_logical_translator::BwiLogicalTranslator& translator, FuturePoseStamped currentLocation) {
-	float robot_x = currentLocation.getPose().pose.position.x;
-	float robot_y = currentLocation.getPose().pose.position.y;
+std::string getRegion(bwi_logical_translator::BwiLogicalTranslator translator, geometry_msgs::PoseStamped currentLocation) {
+	float robot_x = currentLocation.pose.position.x;
+	float robot_y = currentLocation.pose.position.y;
+	ROS_INFO("X: %f Y: %f\n", robot_x, robot_y); 
 	
 	bwi_mapper::Point2f mapPoint(robot_x, robot_y);
-	ROS_INFO("mapPoint created. Found region %s", translator.getLocationString(translator.getLocationIdx(mapPoint)).c_str());	
+	int index = translator.getLocationIdx(mapPoint);
+	ROS_INFO("IDX: %d\n", index);
+	std::string region = (translator.getLocationString(index)).c_str();
+	ROS_INFO("mapPoint created. Found region %s", region);	
+	return region;
 }
 
 int main (int argc, char** argv) {
@@ -33,13 +39,15 @@ int main (int argc, char** argv) {
 	ros::Subscriber sub = n.subscribe("/initialpose", 100, &FuturePoseStamped::setFromPoseWithCovarianceStamped, &initialPose);
 	ros::Subscriber sub1 = n.subscribe("/move_base_interruptable_simple/goal", 100, &FuturePoseStamped::setFromPoseStamped, &goalPose);
 
-	ros::param::set("~map_file", "/home/fri/tgiFRidays_ws/src/verbal_navigation/src/3ne/3ne.yaml");
-	ros::param::set("~data_directory", "/home/fri/tgiFRidays_ws/src/verbal_navigation/src/3ne");
+	ros::param::set("~map_file", "/home/users/fri/TGI_FRIdays_ws/src/verbal_navigation/src/3ne/3ne.yaml");
+	ros::param::set("~data_directory", "/home/users/fri/TGI_FRIdays_ws/src/verbal_navigation/src/3ne");
 	bwi_logical_translator::BwiLogicalTranslator translator;
 	translator.initialize();
 
 
 	while(!(initialPose.isAvailable() && goalPose.isAvailable()) && ros::ok()) ros::spinOnce();
+
+ 	ROS_INFO("%s\n", getRegion(translator, initialPose.getPose()));
 
 	//getRegion(translator, initialPose);
 	ROS_INFO("HAVE START AND GOAL");
@@ -58,6 +66,24 @@ int main (int argc, char** argv) {
 	std::vector<geometry_msgs::PoseStamped> pose_list = srv.response.plan.poses;
 
 	std::vector<std::string> region_list;
+
+	for (int i = 0; i < pose_list.size(); i++) {
+		//std::string region = getRegion(translator, pose_list.at(i));
+		geometry_msgs::PoseStamped currentLocation = pose_list.at(i);
+		float robot_x = currentLocation.pose.position.x;
+		float robot_y = currentLocation.pose.position.y;
+		ROS_INFO("X: %f Y: %f\n", robot_x, robot_y); 
+		
+		//bwi_mapper::Point2f mapPoint(robot_x, robot_y);
+		std::string region = (translator.getLocationString(translator.getLocationIdx(bwi::Point2f(robot_x, robot_y))));
+		ROS_INFO("mapPoint created. Found region %s", region);
+			if (region.compare(region_list.back()) != 0)
+				region_list.push_back(region); 
+	}
+
+	for (int j = 0; j < region_list.size(); j++) {
+		ROS_INFO ("Region: %s\n", region_list.at(j));
+	}
 
 
 	// TODO: pass srv.response into new function (compartmentalize!!)
