ffa41ede41b674bf802fe07ff61c9b6c43383a91
jdeans <jdeans289@gmail.com>
2019-04-09 15:45:36 -0500

    Display basic arrow on screen

--- src/RobotPlanExecutor.cpp
+++ src/RobotPlanExecutor.cpp
@@ -5,13 +5,16 @@
 #include "verbal_navigation/Wavenet.h"
 #include <sound_play/sound_play.h>
 #include <unistd.h>
+#include "visualization_msgs/Marker.h"
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
 #include "verbal_navigation/Robot_Action.h"
 
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 MoveBaseClient* move_client;
+ros::Publisher vis_pub;
 ros::ServiceClient speech_client;
+
 int goto_location(geometry_msgs::Pose dest_pose);
 
 void planCallback(const verbal_navigation::Robot_Action& msg) {
@@ -61,23 +64,73 @@ int goto_location(geometry_msgs::Pose dest_pose) {
    return 1;
 }
 
+void display_arrow () {
+   ROS_INFO("Drawing arrow");
+   visualization_msgs::Marker marker;
+    marker.header.frame_id = "/map";
+    marker.header.stamp = ros::Time::now();
+
+    // Set the namespace and id for this marker.  This serves to create a unique ID
+    // Any marker sent with the same namespace and id will overwrite the old one
+    marker.ns = "basic_shapes";
+    marker.id = 0;
+
+    // Set the marker type.  Initially this is CUBE, and cycles between that and SPHERE, ARROW, and CYLINDER
+    marker.type = visualization_msgs::Marker::ARROW;
+
+    // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
+    marker.action = visualization_msgs::Marker::ADD;
+
+    // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
+    marker.pose.position.x = 0;
+    marker.pose.position.y = 0;
+    marker.pose.position.z = 0;
+    marker.pose.orientation.x = 0.0;
+    marker.pose.orientation.y = 0.0;
+    marker.pose.orientation.z = 0.0;
+    marker.pose.orientation.w = 1.0;
+
+    // Set the scale of the marker -- 1x1x1 here means 1m on a side
+    marker.scale.x = 1.0;
+    marker.scale.y = 1.0;
+    marker.scale.z = 1.0;
+
+    // Set the color -- be sure to set alpha to something non-zero!
+    marker.color.r = 0.0f;
+    marker.color.g = 1.0f;
+    marker.color.b = 0.0f;
+    marker.color.a = 1.0;
+
+    marker.lifetime = ros::Duration();
+   while (ros::ok()) {
+      vis_pub.publish( marker );
+   }
+}
+
 int main(int argc, char **argv){
    ros::init(argc, argv, "RobotPlanExecutor");
    ros::NodeHandle n("~");
    ros::Subscriber plan_sub = n.subscribe("/robot_plan", 1000, planCallback);
-   
+
+   vis_pub = n.advertise<visualization_msgs::Marker>( "visualization_marker", 0 );
+   ROS_INFO("Drawing arrow");
+   // Set the frame ID and timestamp.  See the TF tutorials for information on these.
+   display_arrow();
+
+
    MoveBaseClient mc("move_base", true);
    move_client = &mc;
-   while(!move_client->waitForServer(ros::Duration(5.0))){
-      ROS_INFO("Waiting for the move_base action server to come up");
-   }
+   // while(!move_client->waitForServer(ros::Duration(5.0))){
+   //    ROS_INFO("Waiting for the move_base action server to come up");
+   // }
    ROS_INFO("MoveBase Client Found!");
 
 
    speech_client = n.serviceClient<verbal_navigation::Wavenet>("/wavenet");
-   speech_client.waitForExistence();
+   //speech_client.waitForExistence();
 	ROS_INFO("Speech Client Found!");
 
+
    ros::spin();
-   
+
 }
